21f6355755d7254b829425421c68c901ea5287ab
RainyRedd <101050919+RainyRedd@users.noreply.github.com>
2025-04-29 12:18:26 +0200

    BootStrap D1,D2,B1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TECHCOOL.UI;
+new Database();
+Database.Instance.AddCompany(new() {Name = "Test", Street = "Test", StreetNumber = "1", City = "Test", Contry = "Test", Currency = Currency.DKK});
+Database.Instance.AddCompany(new() {Name = "Test2", Street = "Test2", StreetNumber = "2", City = "Test2", Contry = "Test2", Currency = Currency.EUR});
+
+CompanyListPage companylistpage = new();
+Screen.Display(companylistpage);

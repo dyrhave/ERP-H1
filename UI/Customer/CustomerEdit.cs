bc7bbe29f6272fba9d07993f911012cdfbfbb9a3
dyrhave <rasmus@dyrhave.com>
2025-05-02 11:06:50 +0200

    K2 update - Add customer edit screen

---
+++ UI/Customer/CustomerEdit.cs
@@ -0,0 +1,22 @@
+using TECHCOOL.UI;
+
+public class CustomerEdit : Screen
+{
+    public override string Title { get; set; } = "Edit Customer";
+    protected override void Draw()
+    {
+        Clear();
+        Customer c = new Customer();
+        
+        Form<Customer> editor = new Form<Customer>();
+        editor.TextBox("First Name", nameof(Customer.FirstName));
+        editor.TextBox("Last Name", nameof(Customer.LastName));
+        editor.TextBox("Email", nameof(Customer.Email));
+        editor.TextBox("Street", nameof(Customer.Street));
+        editor.TextBox("Street Number", nameof(Customer.StreetNumber));
+        editor.TextBox("City", nameof(Customer.City));
+        editor.TextBox("PostCode", nameof(Customer.PostCode));
+        editor.TextBox("Country", nameof(Customer.Country));
+        editor.TextBox("CustomerId", nameof(Customer.CustomerId));
+    }
+}

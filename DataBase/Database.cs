2954bc27a4927b624d38d8bf3796e723ef1b4932
dyrhave <rasmus@dyrhave.com>
2025-05-20 15:21:25 +0200

    add connection string prototype

--- DataBase/Database.cs
+++ DataBase/Database.cs
@@ -1,9 +1,25 @@
+using Microsoft.Data.SqlClient;
+
 public partial class Database
 {
     private static Database? _instance;
+
     public static Database Instance => _instance ??= new Database();
+    
     private Database()
     {
-        // Connection string to db
+        // Singleton constructor
+    }
+
+    public SqlConnection GetConnection()
+    {
+        SqlConnectionStringBuilder sb = new();
+        sb.DataSource = "localhost";
+        sb.InitialCatalog = "database";
+        sb.UserID = "bruger";
+        sb.Password = "nogethemmeligt";
+
+        string connectionString = sb.ConnectionString;
+        return new SqlConnection(connectionString);
     }
 }
